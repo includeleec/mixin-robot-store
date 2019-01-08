91bde2015b951c013188a13a858cc4c5aaae5ecd
leec <includeleec@gmail.com>
2019-01-08 17:16:21 +0800

    add ws to application

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from lib.mixin_ws_api import MIXIN_WS_API
+import ws_app
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    mixin_ws = MIXIN_WS_API(on_message=ws_app.on_message)
+    mixin_ws.run()
     return "Hello World!"
